7234eb5411dfd5f1e64c8c8583e8389c72f2402b
Rekha47586 <rekha.kumari@i95dev.com>
2022-03-01 05:23:47 -0800

    message

---
+++ Explore/Text/Block/Product/View/Index.php
@@ -0,0 +1,6 @@
+<?php
+namespace Explore\Text\Block\Product\View;
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+}
---
+++ Explore/Text/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'Explore_Text',
+	__DIR__
+);
